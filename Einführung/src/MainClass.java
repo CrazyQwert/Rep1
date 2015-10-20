701cacf40c1606786c58a7cb5da10886da3a58a7
Arvid <a.buschsieweke@outlook.de>
2015-10-20 21:54:33 +0200

    Removed all unnecessary junk to comply with task 1.

--- Einführung/src/MainClass.java
+++ Einführung/src/MainClass.java
@@ -4,14 +4,6 @@ public class MainClass {
 	public static void main(String[] args){
 		System.out.println("Hello World!");	//Output "Hello World!"
 		
-		int x = 10;
-		int y = 5;
-		int result;
-		
-		result = x + y;
-		
-		System.out.println(result);
-		System.out.println("Work, you dumb bitch!");
 	}
 
 }
