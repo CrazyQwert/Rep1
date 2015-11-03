2b06e2b2e2e579cdb132f97fb07f17a16a8fdd1c
Arvid (SP3) <a.buschsieweke@outlook.de>
2015-11-03 17:05:14 +0100

    A program that renders a square the length of which, as well as the used character, is chosen by the user.

---
+++ Einführung/src/vorfuehrAufgabe3/letterSquare.java
@@ -0,0 +1,54 @@
+package vorfuehrAufgabe3;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class letterSquare {
+	
+	public static void main (String[] args) throws java.io.IOException, InterruptedException {
+		
+		BufferedReader tempinput = new BufferedReader( new InputStreamReader(System.in) );
+		
+		String userinput;
+		int squareWidth;
+		char character;
+		char exit = 'n';
+		
+		do {
+			//Program introduction
+			System.out.println("Hello there! You will now get to choose a desired width and character to render a square with.");
+			
+			//Retrieving and saving square dimensions
+			System.out.print("Please enter a square width you feel comfortable with: ");
+			userinput = tempinput.readLine();
+			squareWidth = Integer.parseInt(userinput);
+			
+			//Spacer
+			System.out.println();
+			
+			//Retrieving and saving character square consists of
+			System.out.print("Now choose a character of your liking that will be used to draw the square: ");
+			character = (char) System.in.read();
+			
+			//Spacer
+			System.out.println();
+			
+			//Drawing the square. First for makes sure required lines get drawn.
+			for( int i = 1; i <= squareWidth; i++ ) {
+				//Inner for makes sure the lines contain the required characters
+				for( int x = 1; x <= squareWidth; x++ ) {
+					System.out.print(character + " ");
+				}
+				System.out.println();
+			}
+			
+			//Exit prompt
+			System.out.println();
+			System.out.print("Do you want to exit? (y/n): ");
+			exit = (char) System.in.read();
+			
+			System.out.println(exit);
+			
+		} while( exit != 'y');
+	}
+
+}
