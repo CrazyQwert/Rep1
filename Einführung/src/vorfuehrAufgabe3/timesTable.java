70d28eab20e51686d0cbfc6e403b74e31d1c9069
Arvid <a.buschsieweke@outlook.de>
2015-11-04 10:48:15 +0100

    Completed the timesTable program.

--- Einführung/src/vorfuehrAufgabe3/timesTable.java
+++ Einführung/src/vorfuehrAufgabe3/timesTable.java
@@ -4,13 +4,33 @@ public class timesTable {
 	
 	public static void main(String args[]) {
 		
-		int x;
+		int result;
 		
-		for( int i = 0; i <= 100; i++ ) {
-			
-			System.out.print(i + " ");
-			x = i * i;
-			System.out.println(x);
+		//First for increases first multiplier by 1 every line
+		for( int i = 1; i <= 10; i++ ) {
+			//Second for increases second multiplier by 1 for every step, but resets every line
+			for (int x = 1; x <= 10; x++ ) {
+				result = i * x;
+				
+				//Correcting the spacing for i
+				if( i < 10 ) {
+					System.out.print( " " + i );
+				} else {
+					System.out.print(i);
+				}
+				
+				System.out.print(" * ");
+				
+				//Correcting the spacing for x
+				if( x < 10 ) {
+					System.out.print( " " + x );
+				} else {
+					System.out.print(x);
+				}
+				
+				System.out.print( " = " + result + "	" );
+			}
+			System.out.println();
 		}
 	}
 
