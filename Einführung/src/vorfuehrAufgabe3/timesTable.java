92419fce5b8ed80b702cac7f7063b8c3385c2f0c
Arvid <a.buschsieweke@outlook.de>
2015-11-04 01:51:22 +0100

    slight change to formatting for letterSquare.
    Added new program "timesTable" which is supposed to display the correct
    times table but does not work as of yet.

---
+++ Einführung/src/vorfuehrAufgabe3/timesTable.java
@@ -0,0 +1,17 @@
+package vorfuehrAufgabe3;
+
+public class timesTable {
+	
+	public static void main(String args[]) {
+		
+		int x;
+		
+		for( int i = 0; i <= 100; i++ ) {
+			
+			System.out.print(i + " ");
+			x = i * i;
+			System.out.println(x);
+		}
+	}
+
+}
