9d1b27aea943e822313b1a7a887e152de1bb36a7
Arvid (SP3) <a.buschsieweke@outlook.de>
2016-01-13 15:01:59 +0100

    Finished exercise 7d

--- Einführung/src/vorfuehrAufgabe7/PolygonManagement2.java
+++ Einführung/src/vorfuehrAufgabe7/PolygonManagement2.java
@@ -5,20 +5,43 @@ import java.io.InputStreamReader;
 
 public class PolygonManagement2 {
 	
+
 	public static void main(String[] args) throws java.io.IOException {
 		
 		BufferedReader tempinput = new BufferedReader( 
 				new InputStreamReader(System.in));
 		int amtPoints;
 		Polygon polygon = new Polygon();
-
+		char alphabet = 'A';
+		
+		System.out.println("What is the name of your polygon?");
+		polygon.name = tempinput.readLine();
+		
 		System.out.println("How many points has your polygon?");
 		amtPoints = Integer.parseInt(tempinput.readLine());
+		polygon.points = new Point[amtPoints];
 		
-		for (int i = 0; i < amtPoints; i++) {
-			
-			
+		
+		if (amtPoints > 2) {
+			for (int i = 0; i < amtPoints; i++) {
+				polygon.points[i] = new Point();
+				polygon.points[i].name = alphabet;
+
+				System.out.println("Please enter a X coordinate for point " + alphabet + " within your polygon:");
+				polygon.points[i].x = Integer.parseInt(tempinput.readLine());
+
+				System.out.println("Please enter a Y coordinate for point " + alphabet + " within your polygon:");
+				polygon.points[i].y = Integer.parseInt(tempinput.readLine());
+
+				alphabet++;
+			}
+			System.out.println();
+			System.out.print("The circumference of your polgon \"" + polygon.name + "\" is: ");
+			System.out.println(calcCircumference(polygon));
+		} else {
+			System.out.println("You need at least 3 points so that a circumference can be calculated!");
 		}
+		
 	}
 	
 	static double calcDistance(Point a, Point b) {
